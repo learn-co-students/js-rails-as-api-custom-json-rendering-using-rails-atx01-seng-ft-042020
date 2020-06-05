3a62a3299bc282ca864a6117e5b2f7469f987fb4
marshall405 <marshall.slemp@gmail.com>
2020-06-04 19:36:48 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,37 @@
 class BirdsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: bird.slice(:id, :name, :species)
+    else
+      render json: {message: "Bird not found"}
+    end
+  end
+
+
+  def create 
+
+    bird = Bird.new(bird_params)
+    if bird.valid?
+      bird.save
+    render json: bird, only: [:id, :name, :species]
+    else
+      render json: {errors: bird.errors.full_messages}
+    end 
+  end
+
+
+  private
+
+  def bird_params
+    params.require(:bird).permit(:name, :species)
+  end
+
 end
